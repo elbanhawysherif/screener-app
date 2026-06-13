66aaaf7e8b7cea115f7956da6c87d26f79247da6
elbanhawysherif <elbanhawy@gmail.com>
2026-06-13 22:05:22 +0300

    Add Flask app with /run endpoint

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
+from flask import Flask, jsonify
+from screener import run_screener
 
+app = Flask(__name__)
+
+@app.route("/run", methods=["GET"])
+def run():
+
+    data = run_screener()
+
+    return jsonify({
+        "status": "success",
+        "count": len(data),
+        "data": data
+    })
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
