85a6ff48b8fa4d91a1141e21479f62cec1d24294
elbanhawysherif <elbanhawy@gmail.com>
2026-06-14 12:22:38 +0300

    Update home route response message
    Change home route response from 'API is running' to 'OK'.

--- app.py
+++ app.py
@@ -1,22 +1,17 @@
 from flask import Flask, jsonify
 from screener import run_screener
-import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "API is running"
+    return "OK"
 
 @app.route("/run")
 def run():
     data = run_screener()
+
     return jsonify({
-        "status": "success",
         "count": len(data),
         "results": data
     })
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
