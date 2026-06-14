8f47f5778cdb139714f7f908cb448c5926d9ec66
elbanhawysherif <elbanhawy@gmail.com>
2026-06-14 19:04:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,13 @@ app = Flask(__name__)
 def home():
     return "PRO SIGNAL ENGINE RUNNING"
 
-@app.route("/run")
-def run():
-    return jsonify(run_screener())
+
+@app.route("/run-html")
+def run_html():
+
+    data = run_screener()
+
+    return Response(
+        data["pretty_html"],
+        mimetype="text/html"
+    )
