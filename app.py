f9b39172bd55eb3f6a409e0d8cdf43d79a82913f
elbanhawysherif <elbanhawy@gmail.com>
2026-06-14 12:39:46 +0300

    Update home route and modify run route response

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
 from flask import Flask, jsonify
-from screener import run_screener
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "OK"
+    return "VERSION_999_WORKING"
 
 @app.route("/run")
 def run():
 
-    data = run_screener()
-
     return jsonify({
-        "count": len(data),
-        "results": data
+        "version": "999",
+        "count": 1,
+        "results": [{"symbol": "TEST", "price": 123}]
     })
