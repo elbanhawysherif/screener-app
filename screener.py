47425399c6c2e4db04824e46f9d2cedfbfb6bc5c
elbanhawysherif <elbanhawy@gmail.com>
2026-06-14 18:14:31 +0300

    Update screener.py

--- screener.py
+++ screener.py
@@ -1,2 +1,10 @@
 def run_screener():
-    return {"status": "screener file is running"}
+
+    from screener import get_sp500_symbols
+
+    symbols = get_sp500_symbols()
+
+    return {
+        "count": len(symbols),
+        "sample": symbols[:10]
+    }
